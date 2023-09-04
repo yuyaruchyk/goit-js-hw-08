d70df18af9af6e6133e494474c731361fc5a24c8
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-09-04 23:15:00 +0200

    upd
    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo('vimeo-player');
+
+
+player.on('timeupdate', function (data) {
+    
+    const currentTime = data.seconds;
+    localStorage.setItem("videoplayer-current-time", currentTime);
+   
+
+
+   
+
+
+   
+});
+const savedTime = localStorage.getItem("videoplayer-current-time");
+
+player.setCurrentTime(savedTime).then(function(seconds) {
+   
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+           
+            break;
+
+        default:
+            
+            break;
+    }
+});
