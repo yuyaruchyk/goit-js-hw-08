aae493b64516aa8e5b4e31af1ff0ec415521323c
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-09-04 12:24:48 +0200

    upd
    upd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import 'src/styles/main';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+import { galleryItems } from "./gallery-items.js";
+
+
+const gallery = document.querySelector(".gallery");
+
+const liArray = galleryItems.map(({ preview, original, description }) => {
+  return `
+  <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+
+  `;
+});
+
+const markup = liArray.join("");
+
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+
