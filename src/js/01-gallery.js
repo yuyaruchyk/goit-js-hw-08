1de23d9f2cddb1bf922026a629a4ea33706140db
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-09-04 13:26:48 +0200

    upd
    upd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,24 +1,22 @@
-
-import SimpleLightbox from "simplelightbox";
-import 'src/styles/main';
+import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 
-import { galleryItems } from "./gallery-items.js";
 
+import { galleryItems } from "./gallery-items.js";
 
 const gallery = document.querySelector(".gallery");
 
 const liArray = galleryItems.map(({ preview, original, description }) => {
   return `
   <li class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
-      data-source="${original}"
       alt="${description}"
+       
     />
   </a>
 </li>
@@ -31,3 +29,8 @@ const markup = liArray.join("");
 gallery.insertAdjacentHTML("afterbegin", markup);
 
 
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
