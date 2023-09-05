6eae861322801f60287980be2c7e29592cd74cb9
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-09-05 19:23:53 +0200

    upd
    upd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+
+form.addEventListener("input", saveItems);
+
+function saveItems(event) {
+
+    event.preventDefault();
+
+    const savedInfo = {
+        email: input.value,
+        message: textarea.value,
+
+    };
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(savedInfo)), 500
+
+
+
+}
